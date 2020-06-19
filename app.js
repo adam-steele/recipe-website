6dd0308130708fa0a904ec71269e90c6c6f27278
Adam-Steele <adamsteele@hotmail.co.uk>
2020-06-19 14:49:27 +0100

    adds functionality so button can switch back and forth between uk and us measurements

--- app.js
+++ app.js
@@ -9,9 +9,10 @@
 
 //example 
 
-const classButt = document.getElementById('test2-butt');
+ const classButt = document.getElementById('test-butt');
 
-const gramsConvert2 = function () {
+
+function buttonClick() {
     const gramsClass = document.getElementsByClassName('grams');
     const o2g2 = 28.35;
 
@@ -22,9 +23,25 @@ const gramsConvert2 = function () {
     const ounces2 = gramsNum2 / o2g2;
     
 
-    gramsClass[i].innerHTML = ounces2.toFixed(0) + 'oz';
+    gramsClass[i].innerHTML = ounces2.toFixed(2) + 'oz';
     }
-};
+    this.onclick = notButtonClick; //function reference to nBC
+}
+
+function notButtonClick() {
+    const gramsClass = document.getElementsByClassName('grams');
+    const o2g2 = 28.35;
 
 
-classButt.addEventListener ('click', gramsConvert2);
+    for (let i = 0; i < gramsClass.length; i++){
+
+    const gramsNum2 = parseFloat(gramsClass[i].textContent);
+    const ounces2 = gramsNum2 * o2g2;
+    
+
+    gramsClass[i].innerHTML = ounces2.toFixed(0) + 'g';
+    }
+    this.onclick = buttonClick; //function reference to original function
+}
+
+classButt.onclick = buttonClick; //again, function reference, no ()
